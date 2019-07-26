aa9ed34c62cff2f90bd8ebff7f618e8764b42d89
Austin D, Latture <austin.latture@gmail.com>
2019-07-26 00:01:41 -0400

    Shows photos when markers tapped

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'package:google_maps_webservice/places.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:flutter/material.dart';
@@ -27,6 +28,8 @@ class MapPageState extends State<MapPage> {
   String errorMessage;
   bool _pressed = false;
   String _placeId;
+  int radius = 2500;
+  Map markerMap = new HashMap<String, String>();
  
   @override
   Widget build(BuildContext context) {
@@ -108,147 +111,158 @@ class MapPageState extends State<MapPage> {
  
   void _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
-    refresh();
-  }
- 
-  Future <LatLng> getUserLocation() async {
-    var currentLocation = <String, double>{};
-    final location = LocationManager.Location();
-    try {
-      currentLocation = await location.getLocation();
-      final lat = currentLocation["latitude"];
-      final lng = currentLocation["longitude"];
-      final center = LatLng(lat, lng);
-      return center;
-    } on Exception {
-      currentLocation = null;
-      return null;
-    }
-  }
-    void refresh() async {
-    final center = await getUserLocation();
-    //Hacky workaround to center the camera. 
-    //TODO: Check for the resolution to this bug in Google Maps iOS SDK
-    mapController.moveCamera(CameraUpdate.newLatLng(center));
-    mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        zoom: 15.0, target: center)));
-    getNearbyPlaces(center);
-  }
- 
-  void showPhotoBox(String placeId) {
-    if (placeId != _placeId) {
-      setState(() {
-        _placeId = placeId;
-        _pressed = true;
-      });
-    }
-  }
- 
-  ListView buildPlacesList() {
-    final placesWidget = places.map((f) {
-      List<Widget> list = [
-        Padding(
-          padding: EdgeInsets.only(bottom: 4.0),
-          child: Text(
-            f.name,
-            style: Theme.of(context).textTheme.subtitle,
-          ),
-        )
-      ];
-      if (f.formattedAddress != null) {
-        list.add(Padding(
-          padding: EdgeInsets.only(bottom: 2.0),
-          child: Text(
-            f.formattedAddress,
-          ),
-        ));
+    mapController.onMarkerTapped.add(_onMarkerTapped);
+        refresh();
       }
-      if (f.vicinity != null) {
-        list.add(Padding(
-          padding: EdgeInsets.only(bottom: 2.0),
-          child: Text(
-            f.vicinity,
-          ),
-        ));
+  
+      Future <LatLng> getUserLocation() async {
+        var currentLocation = <String, double>{};
+        final location = LocationManager.Location();
+        try {
+          currentLocation = await location.getLocation();
+          final lat = currentLocation["latitude"];
+          final lng = currentLocation["longitude"];
+          final center = LatLng(lat, lng);
+          return center;
+        } on Exception {
+          currentLocation = null;
+          return null;
+        }
       }
-      return Padding(
-        padding: EdgeInsets.only(top: 1.0, bottom: 1.0, left: 8.0, right: 8.0),
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10))
-          ),
-          child: InkWell(
-            onTap: () {
-              showPhotoBox(f.placeId);
-            },
-            highlightColor: Colors.lightBlueAccent,
-            splashColor: Colors.blueAccent,
-            child: Padding(
-              padding: EdgeInsets.all(2.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: list,
+        void refresh() async {
+        final center = await getUserLocation();
+        //Hacky workaround to center the camera. 
+        //TODO: Check for the resolution to this bug in Google Maps iOS SDK
+        mapController.moveCamera(CameraUpdate.newLatLng(center));
+        mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            zoom: 15.0, target: center)));
+        getNearbyPlaces(center);
+      }
+     
+      void showPhotoBox(String placeId) {
+        if (placeId != _placeId) {
+          setState(() {
+            _placeId = placeId;
+            _pressed = true;
+          });
+        }
+      }
+     
+      ListView buildPlacesList() {
+        final placesWidget = places.map((f) {
+          List<Widget> list = [
+            Padding(
+              padding: EdgeInsets.only(bottom: 4.0),
+              child: Text(
+                f.name,
+                style: Theme.of(context).textTheme.subtitle,
+              ),
+            )
+          ];
+          if (f.formattedAddress != null) {
+            list.add(Padding(
+              padding: EdgeInsets.only(bottom: 2.0),
+              child: Text(
+                f.formattedAddress,
+              ),
+            ));
+          }
+          if (f.vicinity != null) {
+            list.add(Padding(
+              padding: EdgeInsets.only(bottom: 2.0),
+              child: Text(
+                f.vicinity,
+              ),
+            ));
+          }
+          return Padding(
+            padding: EdgeInsets.only(top: 1.0, bottom: 1.0, left: 8.0, right: 8.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
+              child: InkWell(
+                onTap: () {
+                  showPhotoBox(f.placeId);
+                },
+                highlightColor: Colors.lightBlueAccent,
+                splashColor: Colors.blueAccent,
+                child: Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: list,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      );
-    }).toList();
- 
-    return ListView(shrinkWrap: true, children: placesWidget);
-  }
-  
-    void getNearbyPlaces(LatLng center) async {
-    setState(() {
-      this.isLoading = true;
-      this.errorMessage = null;
-    });
- 
-    final location = Location(center.latitude, center.longitude);
-    final result = await _places.searchNearbyWithRadius(location, 2500);
-    setState(() {
-      this.isLoading = false;
-      if (result.status == "OK") {
-        this.places = result.results;
-        result.results.forEach((f) {
-          final markerOptions = MarkerOptions(
-              position:
-                  LatLng(f.geometry.location.lat, f.geometry.location.lng),
-              infoWindowText: InfoWindowText("${f.name}", "${f.types?.first}"));
-          mapController.addMarker(markerOptions);
-        });
-      } else {
-        this.errorMessage = result.errorMessage;
+          );
+        }).toList();
+     
+        return ListView(shrinkWrap: true, children: placesWidget);
       }
-    });
+      
+        void getNearbyPlaces(LatLng center) async {
+        setState(() {
+          this.isLoading = true;
+          this.errorMessage = null;
+        });
+     
+        final location = Location(center.latitude, center.longitude);
+        final result = await _places.searchNearbyWithRadius(location, radius);
+        setState(() {
+          this.isLoading = false;
+          if (result.status == "OK") {
+            this.places = result.results;
+            result.results.forEach((f) {          
+              final markerOptions = MarkerOptions(
+                  position:
+                    LatLng(f.geometry.location.lat, f.geometry.location.lng),
+                    infoWindowText: InfoWindowText("${f.name}", "${f.types?.first}"),             
+                  );
+    
+              mapController.addMarker(markerOptions).then((marker) => 
+                markerMap.putIfAbsent(marker.id, () => f.placeId)
+              );
+            }
+          );
+          } else {
+            this.errorMessage = result.errorMessage;
+          }
+        });
+      }
+     
+      void onError(PlacesAutocompleteResponse response) {
+        homeScaffoldKey.currentState.showSnackBar(
+          SnackBar(content: Text(response.errorMessage)),
+        );
+      }
+    
+      Future <void> _handlePressButton() async {
+        try {
+          final center = await getUserLocation();
+          Prediction p = await PlacesAutocomplete.show(       
+              context: context,
+              strictbounds: center == null ? false : true,
+              apiKey: kGoogleApiKey,
+              onError: onError,
+              mode: Mode.fullscreen,
+              language: "en",
+              location: center == null
+                  ? null
+                  : Location(center.latitude, center.longitude),
+              radius: center == null ? null : 10000);
+              print("handlePressButton");
+              
+          showPhotoBox(p.placeId);
+        } catch (e) {
+          return;
+        }
+      }
+    
+      void _onMarkerTapped(Marker marker) {
+        print(markerMap);
+        return showPhotoBox(markerMap[marker.id]);
+      }
   }
- 
-  void onError(PlacesAutocompleteResponse response) {
-    homeScaffoldKey.currentState.showSnackBar(
-      SnackBar(content: Text(response.errorMessage)),
-    );
-  }
-
-  Future <void> _handlePressButton() async {
-    try {
-      final center = await getUserLocation();
-      Prediction p = await PlacesAutocomplete.show(       
-          context: context,
-          strictbounds: center == null ? false : true,
-          apiKey: kGoogleApiKey,
-          onError: onError,
-          mode: Mode.fullscreen,
-          language: "en",
-          location: center == null
-              ? null
-              : Location(center.latitude, center.longitude),
-          radius: center == null ? null : 10000);
-          print("handlePressButton");
-          
-      showPhotoBox(p.placeId);
-    } catch (e) {
-      return;
-    }
-  }
-}
